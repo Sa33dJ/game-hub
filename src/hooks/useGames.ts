90f34fda2a2c8b09c479f362a638d09c0a7a9587
Saeed <saeed.jrad.2020@gmail.com>
2023-07-25 02:46:36 +0300

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -5,7 +5,7 @@ export interface Platform {
   id: number;
   name: string;
   slug: string;
-  metacritic: number ;
+  metacritic?: number ;
 }
 
 export interface Game {
@@ -16,6 +16,10 @@ export interface Game {
     parent_platforms: {platform: Platform}[]; 
   }
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id} }, [selectedGenre?.id])
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => useData<Game>(
+  '/games', 
+  {params: {genres: selectedGenre?.id, platforms: selectedPlatform?.id} }, 
+  [selectedGenre?.id, selectedPlatform?.id]
+  )
 
 export default useGames
